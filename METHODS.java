4958d1df3ae03215cea7b032eae2b2e012f63dbe
KohanMilan <75611389+KohanMilan@users.noreply.github.com>
2021-01-22 10:24:25 +0100

    kesz

---
+++ METHODS.java
@@ -0,0 +1,17 @@
+
+package methods;
+
+public class METHODS {
+
+    public METHODS() {
+       static void tagolas(){
+           System.out.println("\n-------------------------------------------\n");
+       }   
+    public static void main(String[] args) {
+        tagolas();
+        System.out.println("A program neve");
+        tagolas();
+  
+    }
+    
+}
